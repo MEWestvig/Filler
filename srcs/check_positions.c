b4a7a81b9d09d74307a75a71e3e8e926efba04e8
Meghan Westvig <mwestvig@student.wethinkcode.co.za>
2018-07-10 11:48:33 +0200

    Updating check_pos

--- srcs/check_positions.c
+++ srcs/check_positions.c
@@ -6,12 +6,28 @@
 /*   By: mwestvig <m.westvig@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 15:39:55 by mwestvig          #+#    #+#             */
-/*   Updated: 2018/07/09 17:02:19 by mwestvig         ###   ########.fr       */
+/*   Updated: 2018/07/10 11:48:08 by mwestvig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int		legal_pos(t_map *map, t_piece *piece, int i, int j)
+{
+}
+
 int		count_pos(t_map *map, t_piece *piece)
 {
+	int num_pos;
+	int i;
+	int j;
+
+	num_pos = 0;
+	i = 0;
+	j = 0;
+	while (i++ < map->map_y)
+		while (j++ < map->map_x)
+			if (legal_pos(map, piece, i, j))
+				num_pos++;
+	return (num_pos);
 }
 
 void	set_positions(t_map *map, t_piece *piece)
